d1f72f4084c7a4778295a17fad9ff44beafe3bf3
Thomas Stromberg <t+github@chainguard.dev>
2025-03-06 03:23:44 -0500

    Improve OpenVMS simulation

--- pkg/personality/openvms.go
+++ pkg/personality/openvms.go
@@ -1,5 +1,10 @@
 package personality
 
+import (
+	"fmt"
+	"strings"
+)
+
 type OpenVMS struct {
 	NodeConfig NodeConfig
 }
@@ -9,16 +14,44 @@ func (p OpenVMS) Name() string {
 }
 
 func (p OpenVMS) Hints() string {
-	return `This is a machine running the latest patch release of OpenVMS for VAX (7.3)
+	node := strings.ToUpper(p.Hostname())
+	return fmt.Sprintf(`This is a machine running the latest patch release of OpenVMS for VAX (7.3)
 - This machine is an VAX 7000 running OpenVMS by Digital Equipment Corporation
 - You are not running UNIX, you are running OpenVMS
-- Use VMS shell conventions,
-- This system only has programs that are documented to come with VMS. If it doesn't have an VMS manpage, it doesn't exist on this system.
+- Your nade name is %s
+- Use VMS DCL (Digital Command Language) shell conventions.
+- This system only has programs that are documented to come with VMS. If it isn't mentioned in the OpenVMS systems manual or VSI OpenVMS wiki, it doesn't exist on this system.
 - This system only understands VMS commands. It does not understand UNIX.
 - Instead of the "ls" command to show a file list, OpenVMS uses DIRECTORY
-- Instead of the "ps" command to show processes, OpenVMS uses "SHOW SYSTEM"
+- Instead of the "ps" command to show processes, OpenVMS uses "SHOW SYSTEM". All process names should be UPPERCASE.
+- Example process names: SECURITY_SERVER, JOB_CONTROL, CRONTAB_MANAGER
 - OpenVMS commands are always in CAPITAL LETTERS. lowercase commands are translated to uppercase.
-`
+- https://antapex.org/vms.txt provides a good guide of what OpenVMS command output looks like
+- The OpenVMS shell lets you define a variable in the form of: VARIABLE_NAME = 10
+- The OpenVMS shell lets you look up the value of a variable with: SHOW SYMBOL VARIABLE_NAME
+- The "SHOW NETWORK" command shows what network protocols are installed, for example, DECNET and TCPIP, as well as network addresses. Here isn example output from a node named SNORRY:
+
+Product:  DECNET        Node:  SNORRY               Address(es):  1.70
+Product:  TCP/IP        Node:  snorrvijoier.poetry.org Address(es):  10.20.1.20
+
+- The "SHOW CLUSTER" command should work, and output a table like:
+
+View of Cluster from system ID 1094  node: %s
++------------------------------+
+Ś       SYSTEMS      Ś MEMBERS Ś
++--------------------+---------Ś
+Ś  NODE   Ś SOFTWARE Ś  STATUS Ś
++---------+----------+---------Ś
+Ś %-7.7s Ś VMS V7.2 Ś         Ś
++------------------------------+
+
+- The "SHOW DEFAULT" command shows the device and directory you are in. For example, for the user "luther", this should be returned by default: "SYS_USERS:[LUTHER]"
+- CREATE/DIR creates a new subdirectory.
+- DIR or DIRECTORY lists files in a directory.
+- HELP <command> should show OpenVMS documentation about a command. For example "HELP DIR" shows help for the DIR command.
+- PRINT filename - This command will queue the file for printing to your standard output queue.
+- SHOW SYMBOL * shows all variables the user has defined within their DCL shell.
+`, node, node, node)
 }
 
 func (p OpenVMS) ShellPrompt() string {
@@ -36,5 +69,5 @@ func (p OpenVMS) Hostname() string {
 	if p.NodeConfig.Hostname != "" {
 		return p.NodeConfig.Hostname
 	}
-	return "vaxen"
+	return "VAXEN"
 }
